bc4e76a97b11c5a3222d5a19088a4ca79f44810c
pc <e0xextazy@gmail.com>
2023-11-21 17:19:17 +0300

    add tests

--- tests/api/test_views.py
+++ tests/api/test_views.py
@@ -5,6 +5,8 @@ from starlette.testclient import TestClient
 from service.settings import ServiceConfig
 
 GET_RECO_PATH = "/reco/{model_name}/{user_id}"
+MODEL_NAME = "simple_model"
+AUTH_TOKEN = "AUTH_TOKEN_123"
 
 
 def test_health(
@@ -20,9 +22,9 @@ def test_get_reco_success(
     service_config: ServiceConfig,
 ) -> None:
     user_id = 123
-    path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
+    path = GET_RECO_PATH.format(model_name=MODEL_NAME, user_id=user_id)
     with client:
-        response = client.get(path)
+        response = client.get(path, params={"token": AUTH_TOKEN})
     assert response.status_code == HTTPStatus.OK
     response_json = response.json()
     assert response_json["user_id"] == user_id
@@ -34,8 +36,41 @@ def test_get_reco_for_unknown_user(
     client: TestClient,
 ) -> None:
     user_id = 10**10
-    path = GET_RECO_PATH.format(model_name="some_model", user_id=user_id)
+    path = GET_RECO_PATH.format(model_name=MODEL_NAME, user_id=user_id)
     with client:
-        response = client.get(path)
+        response = client.get(path, params={"token": AUTH_TOKEN})
     assert response.status_code == HTTPStatus.NOT_FOUND
     assert response.json()["errors"][0]["error_key"] == "user_not_found"
+
+
+def test_get_reco_for_unknown_model(
+    client: TestClient,
+) -> None:
+    user_id = 123
+    path = GET_RECO_PATH.format(model_name="fgdfgdfgdfsdf", user_id=user_id)
+    with client:
+        response = client.get(path, params={"token": AUTH_TOKEN})
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json()["errors"][0]["error_key"] == "model_not_found"
+
+
+def test_get_reco_for_no_auth(
+    client: TestClient,
+) -> None:
+    user_id = 123
+    path = GET_RECO_PATH.format(model_name=MODEL_NAME, user_id=user_id)
+    with client:
+        response = client.get(path)
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+    assert response.json()["errors"][0]["error_key"] == "missing"
+
+
+def test_get_reco_for_bad_auth(
+    client: TestClient,
+) -> None:
+    user_id = 123
+    path = GET_RECO_PATH.format(model_name=MODEL_NAME, user_id=user_id)
+    with client:
+        response = client.get(path, params={"token": "BAD_TOKEN"})
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json()["errors"][0]["error_key"] == "bad_authorization"
